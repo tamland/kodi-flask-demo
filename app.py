7598ccc51de31ee0d1c6a38d6f3fefab0f5ac7b0
Thomas Amland <thomas.amland@gmail.com>
2015-02-19 21:59:53 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Index"
+
+@app.route('/hello')
+def hello():
+    return "Hello!"
+
+if __name__ == '__main__':
+    app.run()
+
